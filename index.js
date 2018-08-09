1e0c30d465a03be9aa54fda8485c5e1ae3b97baa
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-08-09 18:43:16 +0100

    Width height in electron

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const {app, BrowserWindow} = electorn;
 app.on('ready', () => {
     var main_window = new BrowserWindow(
         {
-            fullscreen: true,
+            // fullscreen: true,
+            width: 1024,
+            height: 700,
             backgroundColor: '#312450'
         });
     // main_window.once('ready-to-show', () => {
